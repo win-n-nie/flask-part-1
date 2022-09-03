3477b0374bd35ac8ec8c864cc1b4abae04b8c86d
win-n-nie <102179077+win-n-nie@users.noreply.github.com>
2022-09-02 20:40:34 -0400

    update

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    return 'hello world!!!'
 
-if __name__ == '__main__':
+@app.route('/')
+def about():
+    return 'this is the about us page!'
+
+@app.route('/')
+def contact():
+    return 'haha im not actually going to put my information here'
+if __name__ == '_main_':
     app.run(debug=True, host='localhost', port=80)
+

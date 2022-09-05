f19e1d2673a013ef82adb62068c10decc269345d
win-n-nie <102179077+win-n-nie@users.noreply.github.com>
2022-09-05 12:35:24 -0400

    update

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello_world')
 def hello_world():
     return 'hello world!!!'
 
-@app.route('/')
+@app.route('/about')
 def about():
     return 'this is the about us page!'
 
-@app.route('/')
+@app.route('/contact')
 def contact():
     return 'haha im not actually going to put my information here'
 if __name__ == '_main_':
